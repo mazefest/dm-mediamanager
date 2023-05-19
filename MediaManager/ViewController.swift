38f0b1652285cd60a2823e6b596b651e2dd4500f
Colby Mehmen <colbymehmen@gmail.com>
2023-05-19 05:25:43 -0500

    Initial Commit

---
+++ MediaManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaManager
+//
+//  Created by Colby Mehmen on 5/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
